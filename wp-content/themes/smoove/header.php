83ddcc38d1811421ec71c2ce699d507bb9a50983
AugustoTriste <AT@SG-AT.lan>
2017-03-13 16:57:01 +0000

    Added top navigation for large screens

--- wp-content/themes/smoove/header.php
+++ wp-content/themes/smoove/header.php
@@ -38,23 +38,23 @@
 		<header role="banner">
 				
 			<div class="navbar navbar-fixed-top">
-				<div class="container">
           
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
 
-						<a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
-					</div>
+					<a class="logo" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>">
+						<img src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt="">
+					</a>
+				</div>
 
-					<div class="collapse navbar-collapse navbar-responsive-collapse">
-						<?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
-					</div>
+				<div class="collapse navbar-collapse navbar-responsive-collapse">
+					<?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+				</div>
 
-				</div> <!-- end .container -->
 			</div> <!-- end .navbar -->
 		
 		</header> <!-- end header -->
