c9d5e1c31054114424f12a7861331a8913fc466e
AugustoTriste <AT@SG-AT.lan>
2017-03-14 20:05:13 +0000

    Fixed horizontal scrolling

--- wp-content/themes/smoove/partials/home-sec-2-button.php
+++ wp-content/themes/smoove/partials/home-sec-2-button.php
@@ -1,5 +1,7 @@
-<div class="row text-center sec-2-button">
-    <div class="col-sm-12">
-        <a href="<?php the_sub_field("section_2_app_btn_url"); ?>" class="btn btn-orange" target="_blank">Get the App</a>
-    </div>
+<div class="container">
+	<div class="row text-center sec-2-button">
+	    <div class="col-sm-12">
+	        <a href="<?php the_sub_field("section_2_app_btn_url"); ?>" class="btn btn-orange" target="_blank">Get the App</a>
+	    </div>
+	</div>
 </div>
