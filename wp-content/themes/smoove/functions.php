576fd62475f8ef5ffd826f342147e7825d72d6b0
AugustoTriste <AT@SG-AT.lan>
2017-03-14 17:01:28 +0000

    Remove auto “p” tags

--- wp-content/themes/smoove/functions.php
+++ wp-content/themes/smoove/functions.php
@@ -6,6 +6,10 @@ $locale = get_locale();
 $locale_file = TEMPLATEPATH . "/languages/$locale.php";
 if ( is_readable( $locale_file ) ) require_once( $locale_file );
 
+// Remove P tags
+remove_filter( 'the_content', 'wpautop' );
+remove_filter( 'the_excerpt', 'wpautop' );
+
 // Clean up the WordPress Head
 if( !function_exists( "wp_bootstrap_head_cleanup" ) ) {  
   function wp_bootstrap_head_cleanup() {
@@ -554,7 +558,7 @@ if( !function_exists("wp_bootstrap_theme_styles") ) {
         wp_register_style( 'owl-theme-css', get_template_directory_uri() . '/bower_components/owl/owl-carousel/owl.theme.css', array(), '1.0', 'all' );
         wp_enqueue_style( 'owl-theme-css' );
 
-        wp_register_style( 'wpbs', get_template_directory_uri() . '/library/dist/css/styles.e9bee10b.min.css', array(), '1.0', 'all' );
+        wp_register_style( 'wpbs', get_template_directory_uri() . '/library/dist/css/styles.1595f44b.min.css', array(), '1.0', 'all' );
         wp_enqueue_style( 'wpbs' );
 
         // For child themes
