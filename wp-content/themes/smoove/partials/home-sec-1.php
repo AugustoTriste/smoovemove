024581f0adff650b1a356d4ce79c96a98357fdd1
AugustoTriste <AT@SG-AT.lan>
2017-03-14 21:39:28 +0000

    Fixed columns on homepage section 1

--- wp-content/themes/smoove/partials/home-sec-1.php
+++ wp-content/themes/smoove/partials/home-sec-1.php
@@ -1,7 +1,7 @@
 <div class="section-1 bg-full" style="background-image: url(<?php echo the_sub_field("background_image_1"); ?>);">
     <div class="container-fluid">
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-md-6 col-center">
 				<div class="dis-ta">
 					<div class="text-container dis-cell text-center align-middle">
 						<h2><?php echo the_sub_field("home_main_title_1"); ?></h2>
