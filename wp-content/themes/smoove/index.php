ccdc12a47dfd8ed9bb185dfbed9d9f48ebe22f3a
AugustoTriste <AT@SG-AT.lan>
2017-03-16 23:03:20 +0000

    Blog header - header text styles as global

--- wp-content/themes/smoove/index.php
+++ wp-content/themes/smoove/index.php
@@ -1,5 +1,20 @@
 <?php get_header(); ?>
 
+		<div class="blog-header bg-full" style="background-image: url(<?php the_field('blog_bg_image', get_option('page_for_posts')); ?>);">
+		    <div class="container-fluid">
+				<div class="row">
+					<div class="col-md-6 col-center">
+						<div class="dis-ta header-text">
+							<div class="text-container dis-cell text-center align-middle">
+								<h2><?php the_field('blog_title', get_option('page_for_posts')); ?></h2>
+								<h3><?php the_field('blog_sub_title', get_option('page_for_posts')); ?></h3>
+							</div>
+						</div>
+					</div>
+				</div><!-- /.row -->
+			</div><!-- /.container -->
+		</div><!-- /.section -->
+
 		<div class="blog-section">
 			<div class="container">
 			
--- wp-content/themes/smoove/partials/home-sec-1.php
+++ wp-content/themes/smoove/partials/home-sec-1.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
 		<div class="row">
 			<div class="col-md-6 col-center">
-				<div class="dis-ta">
+				<div class="dis-ta header-text">
 					<div class="text-container dis-cell text-center align-middle">
 						<h2><?php echo the_sub_field("home_main_title_1"); ?></h2>
 						<h3><?php echo the_sub_field("home_sub_title_1"); ?></h3>
