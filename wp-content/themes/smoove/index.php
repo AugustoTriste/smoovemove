11306d6515f2a1d648f15286ae787df5728dbef2
AugustoTriste <AT@SG-AT.lan>
2017-03-18 14:39:43 +0000

    Added isotope filtering on blog page

--- wp-content/themes/smoove/index.php
+++ wp-content/themes/smoove/index.php
@@ -15,80 +15,58 @@
 			</div><!-- /.container -->
 		</div><!-- /.section -->
 
-		<div class="blog-section">
+		<div class="blog-cat">
+			<div class="container">
+				<div class="col-sm-12">
+					<div class="cat-wrap">
+						<ul id="filters" class="list-unstyled list-inline">
+						    <li><a href="#" data-filter="*" class="selected">All</a></li>
+							<?php 
+							 $terms = get_terms("category"); // get all categories, but you can use any taxonomy
+							 $count = count($terms); //How many are they?
+							 if ( $count > 0 ){  //If there are more than 0 terms
+								 foreach ( $terms as $term ) {  //for each term:
+								 echo "<li><a href='#' data-filter='.".$term->slug."'>" . $term->name . "</a></li>\n";
+								 //create a list item with the current term slug for sorting, and name for label
+								 }
+							 } 
+							?>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="blog-section blog-page">
 			<div class="container">
 			
 				<div class="content row posts-row">
 
 					<div class="col-sm-12">
-						<?php 
-				            $startRow = true;
-				            $postCounter = 0;
-				        ?>
-
-						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-						<?php
-					        if ($startRow) {
-					          echo '<!-- START OF INTERNAL ROW --><div class="row post-row">';
-					          $startRow = false;
-					        }  
-				        ?>
-				        <?php $postCounter += 1; ?>
-						
-						<div class="col-sm-4">
+						<?php $the_query = new WP_Query( 'posts_per_page=50' ); //Check the WP_Query docs to see how you can limit which posts to display ?>
+						<?php if ( $the_query->have_posts() ) : ?>
+						    <div id="isotope-list">
+						    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); 
+						 $termsArray = get_the_terms( $post->ID, "category" );  //Get the terms for this particular item
+						 $termsString = ""; //initialize the string that will contain the terms
+						 foreach ( $termsArray as $term ) { // for each term 
+						 $termsString .= $term->slug.' '; //create a string that has all the slugs 
+						 }
+						 ?> 
+						 <div class="col-lg-4 col-md-6 <?php echo $termsString; ?> item">
 					    	<a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
 					    		<?php the_post_thumbnail( 'wpbs-featured' ); ?>
 					    	</a>
 					    	<p class="meta"><?php _e("", "wpbootstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php echo get_the_date('jS M, Y', '','', FALSE); ?></time> <?php _e("/", "wpbootstrap"); ?> <?php the_category(', '); ?>.</p>
 					    	<h4><?php the_title(); ?></h4>
 					    </div>
-
-					     <?php 
-				            if ( 3 === $postCounter ) {
-				                echo '</div><!-- END OF INTERNAL ROW -->';
-				                $startRow = true;
-				                $postCounter = 0;
-				            }
-			            ?>
-						
-						<?php endwhile; ?>
-
-						<?php 
-				            if ($postCounter !== 0 ) {
-				              echo '</div><!-- END OF INTERNAL ROW -->';
-				            }
-			            ?>
+						    <?php endwhile;  ?>
+						    </div> <!-- end isotope-list -->
+						<?php endif; ?>
 					</div>
 
+
 					
-					<?php if (function_exists('wp_bootstrap_page_navi')) { // if expirimental feature is active ?>
-						
-						<?php wp_bootstrap_page_navi(); // use the page navi function ?>
-						
-					<?php } else { // if it is disabled, display regular wp prev & next links ?>
-						<nav class="wp-prev-next">
-							<ul class="pager">
-								<li class="previous"><?php next_posts_link(_e('&laquo; Older Entries', "wpbootstrap")) ?></li>
-								<li class="next"><?php previous_posts_link(_e('Newer Entries &raquo;', "wpbootstrap")) ?></li>
-							</ul>
-						</nav>
-					<?php } ?>		
-					
-					<?php else : ?>
-					
-					<article id="post-not-found">
-					    <header>
-					    	<h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
-					    </header>
-					    <section class="post_content">
-					    	<p><?php _e("Sorry, but the requested resource was not found on this site.", "wpbootstrap"); ?></p>
-					    </section>
-					    <footer>
-					    </footer>
-					</article>
-					
-					<?php endif; ?>
 	    
 				</div> <!-- end #content -->
 
