79b7228ba54614f5c9f7e40a5a784d73502df140
AugustoTriste <AT@SG-AT.lan>
2017-03-19 15:22:13 +0000

    Added collapsible content on default page

--- wp-content/themes/smoove/page.php
+++ wp-content/themes/smoove/page.php
@@ -2,28 +2,26 @@
 
 			<?php
 
-			// check if the flexible content field has rows of data
 			if( have_rows('page_template_content') ):
 
-			     // loop through the rows of data
 			    while ( have_rows('page_template_content') ) : the_row();
 
 			        if( get_row_layout() == 'header_content' ): ?>
 
-				    <div class="section-1 bg-full parallax-header" style="background-image: url(<?php echo the_sub_field('header_background_image'); ?>);">
-						<div class="container-fluid">
-							<div class="row">
-								<div class="col-md-6 col-center">
-									<div class="dis-ta header-text parallax-text">
-										<div class="text-container dis-cell text-center align-middle">
-											<h2 itemprop="headline"><?php the_title(); ?></h2>
-											<h3><?php echo the_sub_field('sub_title'); ?></h3>
-										</div>
-									</div>
+		    <div class="section-1 bg-full parallax-header" style="background-image: url(<?php echo the_sub_field('header_background_image'); ?>);">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-6 col-center">
+							<div class="dis-ta header-text parallax-text">
+								<div class="text-container dis-cell text-center align-middle">
+									<h2 itemprop="headline"><?php the_title(); ?></h2>
+									<h3><?php echo the_sub_field('sub_title'); ?></h3>
 								</div>
-							</div><!-- /.row -->
-						</div><!-- /.container -->
-					</div><!-- /.section -->
+							</div>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.container -->
+			</div><!-- /.section -->
 			
 			<div class="container">
 				<div class="content clearfix row">
@@ -58,27 +56,41 @@
 				</div> <!-- end #content -->
 			</div><!-- /.container -->
 
-			 <?php elseif( get_row_layout() == 'middle_content' ): ?>
+			<?php elseif( get_row_layout() == 'middle_content' ): ?>
 
-		        	<div class="section-3 bg-full" style="background-image: url(<?php echo the_sub_field("background_image"); ?>);">
-					    <div class="container">
-							<div class="row">
-								<div class="col-sm-8 col-center text-center">
-									<div class="dis-ta">
-										<div class="inner-container dis-cell align-middle">
-											<h2><?php echo the_sub_field("title"); ?></h2>
-											<p class="sub-title"><?php echo the_sub_field("sub_title"); ?></p>
-											<div class="button-row">
-												<a href="<?php echo the_sub_field("middle_section_button_url"); ?>" class="btn btn-orange"><?php echo the_sub_field("middle_section_button_text"); ?></a>
-											</div>
-										</div>
-									</div><!-- /.dis-ta -->
-								</div><!-- /.col-sm-5 -->
-							</div><!-- /.row -->
-						</div><!-- /.container -->
-					</div><!-- /.section -->
+        	<div class="section-3 bg-full" style="background-image: url(<?php echo the_sub_field("background_image"); ?>);">
+			    <div class="container">
+					<div class="row">
+						<div class="col-sm-8 col-center text-center">
+							<div class="dis-ta">
+								<div class="inner-container dis-cell align-middle">
+									<h2><?php echo the_sub_field("title"); ?></h2>
+									<p class="sub-title"><?php echo the_sub_field("sub_title"); ?></p>
+									<div class="button-row">
+										<a href="<?php echo the_sub_field("middle_section_button_url"); ?>" class="btn btn-orange"><?php echo the_sub_field("middle_section_button_text"); ?></a>
+									</div>
+								</div>
+							</div><!-- /.dis-ta -->
+						</div><!-- /.col-sm-5 -->
+					</div><!-- /.row -->
+				</div><!-- /.container -->
+			</div><!-- /.section -->
 
-		      <?php  endif;
+				
+			<?php elseif( get_row_layout() == 'collapsible_tabs' ): ?>
+
+			<div class="default-section">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12">
+							<?php echo the_sub_field("collapsible"); ?>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.container -->
+			</div><!-- /.default-section -->
+
+
+		    <?php endif;
 
 		    endwhile;
 
