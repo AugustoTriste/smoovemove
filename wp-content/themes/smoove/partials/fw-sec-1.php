8725f26b20a2aa0ef30ca0cefec2d3c998e08823
AugustoTriste <AT@SG-AT.lan>
2017-03-20 20:02:15 +0000

    Added page template full width and header section

---
+++ wp-content/themes/smoove/partials/fw-sec-1.php
@@ -0,0 +1,14 @@
+<div class="section-1 bg-full parallax-header wow fadeIn" style="background-image: url(<?php echo the_sub_field("header_bg_image"); ?>);">
+    <div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6 col-center">
+				<div class="dis-ta header-text parallax-text">
+					<div class="text-container dis-cell text-center align-middle wow fadeInUp" data-wow-duration="1500ms">
+						<h2><?php echo the_sub_field("title"); ?></h2>
+						<h3><?php echo the_sub_field("sub_title"); ?></h3>
+					</div>
+				</div>
+			</div>
+		</div><!-- /.row -->
+	</div><!-- /.container -->
+</div><!-- /.section -->
