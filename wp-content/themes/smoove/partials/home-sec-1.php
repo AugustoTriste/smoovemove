f29301c469a60b1d3d37b0acbe3a196065db2a37
AugustoTriste <AT@SG-AT.lan>
2017-03-20 20:05:19 +0000

    Changed animation speed on homepage section 1

--- wp-content/themes/smoove/partials/home-sec-1.php
+++ wp-content/themes/smoove/partials/home-sec-1.php
@@ -1,9 +1,9 @@
-<div class="section-1 bg-full parallax-header" style="background-image: url(<?php echo the_sub_field("background_image_1"); ?>);">
+<div class="section-1 bg-full parallax-header wow fadeIn" style="background-image: url(<?php echo the_sub_field("background_image_1"); ?>);">
     <div class="container-fluid">
 		<div class="row">
 			<div class="col-md-6 col-center">
 				<div class="dis-ta header-text parallax-text">
-					<div class="text-container dis-cell text-center align-middle wow fadeInUp" data-wow-duration="1s">
+					<div class="text-container dis-cell text-center align-middle wow fadeInUp" data-wow-duration="1500ms">
 						<h2><?php echo the_sub_field("home_main_title_1"); ?></h2>
 						<h3><?php echo the_sub_field("home_sub_title_1"); ?></h3>
 					</div>
