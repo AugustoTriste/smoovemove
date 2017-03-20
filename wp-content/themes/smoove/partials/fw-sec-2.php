c7912193424f6c4dbdc294ccbc4783a76c86d414
AugustoTriste <AT@SG-AT.lan>
2017-03-20 21:40:27 +0000

    Fixed full page width section 2

--- wp-content/themes/smoove/partials/fw-sec-2.php
+++ wp-content/themes/smoove/partials/fw-sec-2.php
@@ -1,13 +1,13 @@
-<div class="section-2">
+<div class="section-images">
     <div class="container">
-		<div class="row">
-			<div class="col-md-6 col-center wow fadeInUp" data-wow-duration="1s" data-wow-delay="750ms">
-				<img src="<?php echo the_sub_field("logo"); ?>" alt="">
+		<div class="row wow fadeInUp" data-wow-duration="1s" data-wow-delay="750ms">
+			<div class="col-md-6 col-center">
+				<img src="<?php echo the_sub_field("logo"); ?>" alt="Smoovemove">
 				<h2><?php echo the_sub_field("title"); ?></h2>
 				<p><?php echo the_sub_field("sub_title"); ?></p>
 			</div>
 		</div><!-- /.row -->
-		<div class="row hidden-xs">
+		<div class="row images-row hidden-xs wow fadeInUp" data-wow-duration="1s" data-wow-delay="750ms">
 		 <?php
             if(have_rows("images_row")):
                 while(have_rows("images_row")): the_row();
@@ -18,7 +18,7 @@
 	                    <?php
 	                    if(!empty($column_image)):
 	                    ?>
-	                        <img src="<?php the_sub_field("image"); ?>">
+	                        <img class="round" src="<?php the_sub_field("image"); ?>">
 	                    <?php
 	                    endif;
 	                    ?>
@@ -31,7 +31,7 @@
         ?>
         </div><!-- /.row -->
 		
-		<div class="row visible-xs owl-carousel owl-theme">
+		<div class="row images-row visible-xs owl-carousel owl-theme wow fadeInUp" data-wow-duration="1s" data-wow-delay="750ms">
 		 <?php
             if(have_rows("images_row")):
                 while(have_rows("images_row")): the_row();
@@ -42,7 +42,7 @@
 	                    <?php
 	                    if(!empty($column_image)):
 	                    ?>
-	                        <img src="<?php the_sub_field("image"); ?>">
+	                        <img class="round" src="<?php the_sub_field("image"); ?>">
 	                    <?php
 	                    endif;
 	                    ?>
@@ -54,7 +54,7 @@
             endif;
         ?>
         </div><!-- /.row -->
-        <div class="row text-center sec-2-button wow fadeInUp" data-wow-duration="1s" data-wow-delay="750ms">
+        <div class="row button-row text-center wow fadeInUp" data-wow-duration="1s" data-wow-delay="750ms">
 		    <div class="col-sm-12">
 		        <a href="<?php the_sub_field("button_url"); ?>" class="btn btn-orange" target="_blank"><?php echo the_sub_field("button_text"); ?></a>
 		    </div>
