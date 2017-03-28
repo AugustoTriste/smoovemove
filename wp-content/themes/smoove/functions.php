508f2ae92f9b5500d47ae0cd867a19b010075155
AugustoTriste <AT@SG-AT.lan>
2017-03-28 23:41:54 +0100

    Fixed spaces on mobile header page template

--- wp-content/themes/smoove/functions.php
+++ wp-content/themes/smoove/functions.php
@@ -459,7 +459,7 @@ if( !function_exists("wp_bootstrap_theme_styles") ) {
         wp_register_style( 'bootstrap-select', get_template_directory_uri() . '/bower_components/bootstrap-select/dist/css/bootstrap-select.min.css', array(), '1.0', 'all' );
         wp_enqueue_style( 'bootstrap-select' );
 
-        wp_register_style( 'wpbs', get_template_directory_uri() . '/library/dist/css/styles.a2f725d2.min.css', array(), '1.0', 'all' );
+        wp_register_style( 'wpbs', get_template_directory_uri() . '/library/dist/css/styles.6ebc5fa8.min.css', array(), '1.0', 'all' );
         wp_enqueue_style( 'wpbs' );
 
         // For child themes
